91528c57f4c57c7d60ce94fcb5af189aac131472
Andrea <andrea3aprile99@gmail.com>
2021-06-15 12:38:51 +0200

    rotte

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::prefix('/')->namespace('UI')->group(function () {
 // Route::get('/home', 'HomeController@index'); Pagina Login
 
 
+
 Auth::routes();
 
 Route::prefix('ur')->name('ur.')->namespace('UR')->middleware('auth')->group(function () {
