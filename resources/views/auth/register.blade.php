3856ffdb73905d2930e822ade001c903cbf9ac28
Caterina Bertotti <bertotti4997@gmail.com>
2021-06-15 13:08:39 +0200

    Add Custom Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,34 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Lastname') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
+
+                                @error('last_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="birthday" class="col-md-4 col-form-label text-md-right">{{ __('Birthday') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="birthday" type="date" class="form-control @error('birthday') is-invalid @enderror" name="birthday" value="{{ old('birthday') }}" required autocomplete="birthday" autofocus>
+
+                                @error('birthday')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
--- resources/views/ur/apartments/index.blade.php
+++ resources/views/ur/apartments/index.blade.php
@@ -1,8 +1,116 @@
-@extends('layouts.base')
+@extends('layouts.ur-base')
 
 @section('pageTitle')
     BoolBnb - Dashboard
 @endsection
 
-@section('mainContent')
+@section('urMainContent')
+    <div class="dash-wrapper">
+        <div class="dash__card">
+            <a href="#">
+                <!-- card top -->
+                <div class="img__wrapper">
+                    <img src="#" alt="placeholder">
+                </div>
+                <!-- / card top -->
+                <!-- card bottom -->
+                <div class="card__caption">
+                    <h4>Titolo Prova</h4>
+                    <p class="card__caption__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, atque assumenda? Autem at officiis exercitationem fugiat, dignissimos enim impedit veritatis pariatur vitae labore quia maxime in quae deleniti sint et.</p>
+                </div>
+                <!-- card bottom -->
+            </a>
+        </div>
+        <div class="dash__card">
+            <a href="#">
+                <!-- card top -->
+                <div class="img__wrapper">
+                    <img src="#" alt="placeholder">
+                </div>
+                <!-- / card top -->
+                <!-- card bottom -->
+                <div class="card__caption">
+                    <h4>Titolo Prova</h4>
+                    <p class="card__caption__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, atque assumenda? Autem at officiis exercitationem fugiat, dignissimos enim impedit veritatis pariatur vitae labore quia maxime in quae deleniti sint et.</p>
+                </div>
+                <!-- card bottom -->
+            </a>
+        </div>
+        <div class="dash__card">
+            <a href="#">
+                <!-- card top -->
+                <div class="img__wrapper">
+                    <img src="#" alt="placeholder">
+                </div>
+                <!-- / card top -->
+                <!-- card bottom -->
+                <div class="card__caption">
+                    <h4>Titolo Prova</h4>
+                    <p class="card__caption__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, atque assumenda? Autem at officiis exercitationem fugiat, dignissimos enim impedit veritatis pariatur vitae labore quia maxime in quae deleniti sint et.</p>
+                </div>
+                <!-- card bottom -->
+            </a>
+        </div>
+        <div class="dash__card">
+            <a href="#">
+                <!-- card top -->
+                <div class="img__wrapper">
+                    <img src="#" alt="placeholder">
+                </div>
+                <!-- / card top -->
+                <!-- card bottom -->
+                <div class="card__caption">
+                    <h4>Titolo Prova</h4>
+                    <p class="card__caption__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, atque assumenda? Autem at officiis exercitationem fugiat, dignissimos enim impedit veritatis pariatur vitae labore quia maxime in quae deleniti sint et.</p>
+                </div>
+                <!-- card bottom -->
+            </a>
+        </div>
+        <div class="dash__card">
+            <a href="#">
+                <!-- card top -->
+                <div class="img__wrapper">
+                    <img src="#" alt="placeholder">
+                </div>
+                <!-- / card top -->
+                <!-- card bottom -->
+                <div class="card__caption">
+                    <h4>Titolo Prova</h4>
+                    <p class="card__caption__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, atque assumenda? Autem at officiis exercitationem fugiat, dignissimos enim impedit veritatis pariatur vitae labore quia maxime in quae deleniti sint et.</p>
+                </div>
+                <!-- card bottom -->
+            </a>
+        </div>
+        <div class="dash__card">
+            <a href="#">
+                <!-- card top -->
+                <div class="img__wrapper">
+                    <img src="#" alt="placeholder">
+                </div>
+                <!-- / card top -->
+                <!-- card bottom -->
+                <div class="card__caption">
+                    <h4>Titolo Prova</h4>
+                    <p class="card__caption__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, atque assumenda? Autem at officiis exercitationem fugiat, dignissimos enim impedit veritatis pariatur vitae labore quia maxime in quae deleniti sint et.</p>
+                </div>
+                <!-- card bottom -->
+            </a>
+        </div>
+        <div class="dash__card">
+            <a href="#">
+                <!-- card top -->
+                <div class="img__wrapper">
+                    <img src="#" alt="placeholder">
+                </div>
+                <!-- / card top -->
+                <!-- card bottom -->
+                <div class="card__caption">
+                    <h4>Titolo Prova</h4>
+                    <p class="card__caption__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, atque assumenda? Autem at officiis exercitationem fugiat, dignissimos enim impedit veritatis pariatur vitae labore quia maxime in quae deleniti sint et.</p>
+                </div>
+                <!-- card bottom -->
+            </a>
+        </div>
+        
+    </div>
 @endsection
