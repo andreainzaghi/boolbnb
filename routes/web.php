806ad5ea2e6c487985930ca8848301d7157be642
Tommaso P <tommaso.plinio@gmail.com>
2021-06-17 21:20:54 +0200

    scritto admin nella route

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,9 @@ Route::prefix('/')->namespace('UI')->group(function () {
 
 Auth::routes();
 
-Route::prefix('ur')->name('admin.')->namespace('ADMIN')->middleware('auth')->group(function () {
+Route::prefix('admin')->name('admin.')->namespace('ADMIN')->middleware('auth')->group(function () {
     Route::resource('apartments', 'ApartmentController');
+    
 });
 
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
