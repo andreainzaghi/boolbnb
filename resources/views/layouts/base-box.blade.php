fadd9c51f321764ed9d5912296ab7307d1d356f4
Caterina Bertotti <bertotti4997@gmail.com>
2021-06-17 09:06:34 +0200

    pagina edit

--- resources/views/layouts/base-box.blade.php
+++ resources/views/layouts/base-box.blade.php
@@ -3,7 +3,11 @@
 @section('mainLay')
     <div class="main__box box-shadow px-0">
         <div class="ur-wrapper">
-            <h2 class="ur-main__title">@yield('mainTitle')</h2>
+            <div class="titlewrapper">
+                <h2 class="ur-main__title">@yield('mainTitle')</h2>
+                @yield('fixedElement')
+            </div>
+
             @yield('MainContent')
         </div>
     </div>
