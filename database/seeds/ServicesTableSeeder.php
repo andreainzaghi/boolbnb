3a119d0aff2fe445d74971d5c05add683f326f19
giordanoale <a.giordano89@gmail.com>
2021-06-17 16:34:11 +0200

    fix bnbcontroller

--- database/seeds/ServicesTableSeeder.php
+++ database/seeds/ServicesTableSeeder.php
@@ -24,6 +24,59 @@ class ServicesTableSeeder extends Seeder
                 [ 'name' => 'asciugatrice',
                   'icon_class' => 'fas fa-wind' 
                 ],
+                [
+                    'name' => 'seggiolone',
+                    'icon_class' => 'fas fa-baby'
+                ],
+                [
+                    'name' => 'colazione',
+                    'icon_class' => 'fas fa-coffee'
+                ],
+                [
+                    'name' => 'ferro da stiro',
+                    'icon_class' => 'fas fa-tshirt'
+                ],
+                [
+                    'name' => 'spazio di lavoro dedicato',
+                    'icon_class' => 'fas fa-briefcase'
+                ],
+                [
+                    'name' => 'culla',
+                    'icon_class' => 'fas fa-baby-carriage'
+                ],
+                [
+                    'name' => 'self check-in',
+                    'icon_class' => 'fas fa-key'
+                ],
+                [
+                    'name' => 'tv',
+                    'icon_class' => 'fas fa-tv'
+                ],
+                [
+                    'name' => 'lavatrice',
+                    'icon_class' => 'fas fa-soap'
+                ],
+                [
+                    'name' => 'wi-fi',
+                    'icon_class' => 'fas fa-wifi'
+                ],
+                [
+                    'name' => 'riscaldamento',
+                    'icon_class' => 'fas fa-thermometer-full'
+                ],
+                [
+                    'name' => 'asciugacapelli',
+                    'icon_class' => 'fas fa-bell'
+                ],
+                [
+                    'name' => 'allarme antincendio',
+                    'icon_class' => 'fab fa-free-code-camp'
+                ],
+                [
+                    'name' => 'bagno privato',
+                    'icon_class' => 'fas fa-bath'
+                ],
+                
              ];
 
 
@@ -58,7 +111,7 @@ class ServicesTableSeeder extends Seeder
             'fas fa-wind',// asciugatrice
             'fas fa-baby',// seggiolone
             'fas fa-coffee', // colazione
-            '',// ferro da stiro??
+            'fas fa-tshirt',// ferro da stiro?? (ho usato una maglietta)
             'fas fa-briefcase', // spazio di lavoro dedicato
             'fas fa-baby-carriage', // culla
             'fas fa-key',// self check-in
