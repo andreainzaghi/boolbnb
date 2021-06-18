50a32f26075d5a51d38fa20b2ce5cfd98abae4d5
luca <luca.lorenzetti0@gmail.com>
2021-06-18 15:00:08 +0200

    Creazione dei primi controllers
    Mails e pagamenti

---
+++ app/Http/Controllers/Transactions/PaymentController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Transactions;
+
+use App\Apartment;
+use App\Http\Controllers\Controller;
+use App\Sponsor;
+use Braintree\Gateway;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+
+    public function form(Apartment $apartment){
+
+        $sponsor = Sponsor::where('name', 'Silver')->first();
+        $date = Carbon::now()->addHour($sponsor->hours)->isoFormat('DD-MM-YYYY, hh:mm');
+
+        return view('ui.payment', compact('apartment','sponsor','date'));
+        //return redirect( 'apartments/{apartment}/payment/result')->with("");
+    }
+
+    public function process(Request $request, Gateway $gateway)
+    {
+
+        dd("processo pagamento");
+      //  dd($gateway->clientToken()->generate());
+    // $payload = $request->input('payload', false);
+    // $nonce = $payload['nonce'];
+
+    // $status = Braintree\Transaction::sale([
+	// 'amount' => '10.00',
+	// 'paymentMethodNonce' => $nonce,
+	// 'options' => [
+	//     'submitForSettlement' => True
+	// ]
+    // ]);
+
+    //         if ($status->success) {
+    //     print_r("success!: " . $status->transaction->id);
+    // } else if ($status->transaction) {
+    //     print_r("Error processing transaction:");
+    //     print_r("\n  code: " . $status->transaction->processorResponseCode);
+    //     print_r("\n  text: " . $status->transaction->processorResponseText);
+    // } else {
+    //     foreach($status->errors->deepAll() AS $error) {
+    //         print_r($error->code . ": " . $error->message . "\n");
+    //     }
+    // }
+
+    //     return response()->json($status);
+  
+    // }
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,41 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
+// AREA PUBBLICA
 Route::prefix('/')->namespace('UI')->group(function () {
     Route::get('', 'BnbController@index')->name('welcome');
     Route::get('search', 'BnbController@search')->name('search');
     Route::get('apartments/{id}', 'BnbController@show')->name('ui.apartments.show');
 });
+//////////////////////////////////////////////////////////////////////
+
+
+// BRAINTREE - TRANSAZIONI
+Route::prefix('/')->namespace('Transactions')->group(function () {
+    Route::get('apartments/{apartment}/payment', 'PaymentController@form')->name('payment.form');
+    Route::get('apartments/{apartment}/payment/result', 'PaymentController@process')->name('payment.process');
+
+});
+
+// Route::namespace('Transactions')->get('ui/apartments/{apartment}/payment/process', 'PaymentController@process')->name('payment.process');
+// Route::namespace('Transactions')->get('ui/apartments/{apartment}/payment/process', 'PaymentController@process')->name('payment.process');
+//////////////////////////////////////////////////////////////////////
+
 
 // Route::get('/home', 'HomeController@index'); Pagina Login
 
 Auth::routes();
 
+
+// AREA PRIVATA
 Route::prefix('admin')->name('admin.')->namespace('ADMIN')->middleware('auth')->group(function () {
     Route::resource('apartments', 'ApartmentController');
 });
 
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+//////////////////////////////////////////////////////////////////////
+
+
+
+
 
