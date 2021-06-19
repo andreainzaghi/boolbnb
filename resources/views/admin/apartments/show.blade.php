fe5e7a5dd5b36df99e47503710606f9bbd37dcba
Tommaso P <tommaso.plinio@gmail.com>
2021-06-19 11:50:31 +0200

    le show sono resposive

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -12,30 +12,31 @@
 
 @section('MainContent')
 <div class="box">
-  <div class="admin-sponsor-box d-flex sp-btw flex-wrap ">
+  <div class="admin-sponsor-box d-flex sp-btw flex-wrap">
+
     {{-- checkboxes  --}}
     <div class="custom-control custom-radio custom-control-inline">
       <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
-      <label class="custom-control-label" for="customRadioInline1">Silver | 2,99 € per 24 ore</label>
+      <label class="custom-control-label color" for="customRadioInline1"> Silver | 2,99 € per 24 ore</label>
     </div>
     <div class="custom-control custom-radio custom-control-inline">
         <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
-        <label class="custom-control-label" for="customRadioInline2">Gold | 5,99 € per 72 ore</label>
+        <label class="custom-control-label color" for="customRadioInline2">Gold | 5,99 € per 72 ore</label>
     </div>
     <div class="custom-control custom-radio custom-control-inline">
       <input type="radio" id="customRadioInline3" name="customRadioInline1" class="custom-control-input">
       <label class="custom-control-label" for="customRadioInline3">Platinum | 9,99 € per 144 ore</label>
     </div>
     {{-- checkboxes  --}}
+
     {{-- bottone sponsorizza  --}}
-    <div class="buttons-admin">
-      <a href="#" class="my-btn my-btn-primary"><span class=" d-md-inline-block">Sponsorizza</span></a> 
+    <div class="sponsorizza">
+      <a href="#" class="my-btn my-btn-primary "><span class=" d-md-inline-block">Sponsorizza</span></a> 
     </div>
     {{-- bottone sponsorizza  --}}
       
   </div> 
-  
-  
+   
   <div class="admin-appartamento d-flex"> 
 
         <!-- box-left -->
@@ -54,106 +55,17 @@
           <div class="descrizione">
             <p class="recap d-flex">Stanze: 5 | Bagni: 3| Letti: 3 | Ospiti: 2 | Dimensioni: 90mq </p>
             <p class="sottotesto">Lorem ipsum dolor sit amet, consectetur adipisicing elit, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit. Lorem ipsum dolor sit amet, consectetur adipisicing elit, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+            {{-- serivizi --}}
             <div class="select-servizi d-flex-colmn flex-wrap">
               <h4>Servizi</h4> 
-              {{-- checkboxes lato admin --}}
-              <div class="check-squares d-flex flex-wrap sp-btw">
-                <div class="square-1">
-                  Cucina
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-                <div class="square-2">
-                  Wifi
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-                <div class="square-3">
-                  Tv
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-                <div class="square-4">
-                  Lavatrice
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-                <div class="square-5">
-                  Biancheria
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-                <div class="square-6">
-                  Cucina
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-                <div class="square-7">
-                  Superfice
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-              </div>
-              <div class="check-squares d-flex flex-wrap sp-btw">
-                <div class="square-1">
-                  Cucina
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-                <div class="square-2">
-                  Wifi
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-                <div class="square-3">
-                  Tv
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-                <div class="square-4">
-                  Lavatrice
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-                <div class="square-5">
-                  Biancheria
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-                <div class="square-6">
-                  Cucina
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-                <div class="square-7">
-                  Superfice
-                  
-                  <input type="checkbox" name="" value="">
-                  
-                </div>
-              </div>
-              {{-- checkboxes lato admin --}}
+            
 
-            </div>          
+            </div>  
+            {{-- serivizi --}}        
           </div>
         </div>
 
-
-
-
+        <!-- box-right -->
         <div class="box-right d-flex-colmn">
           <div class="admin-title-page">
             <div class="title-map">
@@ -166,10 +78,10 @@
           </div>
         </div>
 
-        <!-- rifaccio -->           
+               
   </div>
   {{-- bottoni lato admin --}}
-  <div class="buttons-admin d-flex flex-wrap sp-btw">
+  <div class="admin-button d-flex flex-wrap sp-btw">
     <a href="#" class="my-btn my-btn-secondary"><span class=" d-md-inline-block">Modifica</span></a> 
     <a href="#" class="my-btn my-btn-secondary"><span class=" d-md-inline-block">Visualizza messaggi</span></a>   
     <a href="#" class="my-btn my-btn-primary"><span class=" d-md-inline-block">Elimina struttura</span></a>  
