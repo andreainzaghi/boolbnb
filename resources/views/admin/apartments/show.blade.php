a0f15d1bd7f040ffbf0ecbc967b36d1db6b1ff54
luca <luca.lorenzetti0@gmail.com>
2021-06-25 11:08:55 +0200

    Add icon visible

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -40,7 +40,7 @@
         <div class="box-left d-flex-colmn">
 
           <div class="title">
-            <h3>{{ $apartment->title }}</h3>
+            <h3>{{ $apartment->title }} @if($apartment->visible) <span><i class="fas fa-eye"></i></span> @else <i class="fas fa-eye-slash"></i>@endif</h3>
             <p id="address">{{ $apartment->address }}, {{ $apartment->city }}</p>
           </div>
 
