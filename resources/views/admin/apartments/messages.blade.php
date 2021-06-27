05070712da53ecaa1b37a98e4e6c1ca7ea00720b
luca <luca.lorenzetti0@gmail.com>
2021-06-27 22:58:33 +0200

    Fix page messages

--- resources/views/admin/apartments/messages.blade.php
+++ resources/views/admin/apartments/messages.blade.php
@@ -4,34 +4,7 @@
     {{ $apartment->title }} - Messaggi ricevuti
 @endsection
 
-@section('styles')
-    <style>
-        .ur-wrapper {
-            align-content: flex-start;
-        }
-        .list-group {
-            width: 50%;
-        }
-        .list-group-item span {
-            overflow: hidden;
-            white-space: nowrap;
-            text-overflow: ellipsis;
-            -webkit-line-clamp: 1;
-            display: inline-block;
-        }
-        .list-item__top {
-            width: 100%;
-            display: flex;
-            justify-content: space-between;
-        }
-        .list-item__bottom {
-            overflow: hidden;
-        }
-        .list-item__bottom span {
-            width: 100%;
-        }
-    </style>
-@endsection
+
 
 @section('mainTitle')
     {{ $apartment->title }} - Messaggi ricevuti
